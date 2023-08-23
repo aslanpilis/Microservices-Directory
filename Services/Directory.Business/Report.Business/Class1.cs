08abad1fd012e83f86d15ac98f0b9cd8dafc02e1
Aslan Pilis <aslanpilis@gmail.com>
2023-08-23 09:41:40 +0300

    creation of necessary projects

---
+++ Services/Directory.Business/Report.Business/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Report.Business
+{
+    public class Class1
+    {
+    }
+}
---
+++ Services/Report/Report.Entities/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Report.Entities
+{
+    public class Class1
+    {
+    }
+}
